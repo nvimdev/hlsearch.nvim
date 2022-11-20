d115a16adc4a81405d9c1bd93dface4fe9d1c45e
Raphael <glephunter@gmail.com>
2022-11-20 19:51:56 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # hlsearch.nvim
-auto remove search highlight and rehighlight when using n or m
+auto remove search highlight and rehighlight when using n or N
+
+## Install
+
+- with packer.nvim
+
+```lua
+packer.use {'glepnir/hlsearch.nvim', event = 'BufRead'}
+```
+
+## Showcase
+
+![images](https://user-images.githubusercontent.com/41671631/202900349-8129cd13-6068-4c07-8e94-9f0cfd15e2df.gif)
+
+## License MIT
---
+++ lua/hlsearch/init.lua
@@ -0,0 +1,60 @@
+local api, fn = vim.api, vim.fn
+local hs = {}
+local group = api.nvim_create_augroup('Hlsearch', { clear = true })
+
+local function stop_hl()
+  if vim.v.hlsearch == 0 then
+    return
+  end
+  local keycode = api.nvim_replace_termcodes('<Cmd>nohl<CR>', true, false, true)
+  api.nvim_feedkeys(keycode, 'n', false)
+end
+
+local function start_hl()
+  local res = fn.getreg('/')
+  if vim.v.hlsearch == 1 and not fn.search([[\%#\zs]] .. res, 'cnW') then
+    stop_hl()
+  end
+end
+
+local function hs_event(bufnr)
+  local cm_id = api.nvim_create_autocmd('CursorMoved', {
+    buffer = bufnr,
+    group = group,
+    callback = function()
+      start_hl()
+    end,
+    desc = 'Auto hlsearch',
+  })
+
+  local ie_id = api.nvim_create_autocmd('InsertEnter', {
+    buffer = bufnr,
+    group = group,
+    callback = function()
+      stop_hl()
+    end,
+    desc = 'Auto remove hlsearch',
+  })
+
+  api.nvim_create_autocmd('BufDelete', {
+    buffer = bufnr,
+    group = group,
+    callback = function(opt)
+      pcall(api.nvim_del_autocmd, cm_id)
+      pcall(api.nvim_del_autocmd, ie_id)
+      pcall(api.nvim_del_autocmd, opt.id)
+    end
+  })
+end
+
+function hs.setup()
+  api.nvim_create_autocmd('BufEnter', {
+    group = group,
+    callback = function(opt)
+      hs_event(opt.buffer)
+    end,
+    desc = 'hlsearch.nvim event'
+  })
+end
+
+return hs
